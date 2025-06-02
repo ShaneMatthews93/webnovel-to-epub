e5ac553b4f8681e721a48d4230f350933a59a025
Shane Matthews <shanematthews93@icloud.com>
2025-06-02 11:17:17 -0700

    Removed Ollama related files

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ from src.batch_scraper import fetch_multi_chapters
 from src.epub_builder import build_epub
 from src.chapter_table_scraper import table_of_content_extractor
 from src.book_manager import book_manager
-from src.ollama_fetch_chapter import fetch_chapter #delete after testing AI
 
 def main():
    
-    #book_manager()
-    
-    url = 'https://novelbin.com/b/library-of-heavens-path/chapter-1'
-    fetch_chapter()
+    book_manager()
     
 if __name__ == "__main__":
     main()
